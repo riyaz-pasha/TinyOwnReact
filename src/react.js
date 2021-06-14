2e19cc499bd558649d926cc1af63e4ebb9447c3e
riyaz <riyazpasha880@gmail.com>
2021-06-14 21:56:30 +0530

    Add. create a div elements and render it

---
+++ src/react.js
@@ -0,0 +1,19 @@
+(() => {
+    function div(children) {
+        const aDiv = document.createElement("div");
+        aDiv.innerHTML = children.join(" ");
+        return aDiv;
+    }
+
+    function createElement(element, props, ...children) {
+        return div(children);
+    }
+
+    window.React = {
+        createElement,
+    }
+
+    window.ReactDOM = {
+        render: (element, domElement) => { domElement.appendChild(element) }
+    }
+})()
