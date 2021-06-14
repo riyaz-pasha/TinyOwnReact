bbb324d10f7e291f95772a65415d65b3f6945288
riyaz <riyazpasha880@gmail.com>
2021-06-14 22:34:02 +0530

    Refactor. Extracted utils into separate file.

--- src/react.js
+++ src/react.js
@@ -1,16 +1,11 @@
 (() => {
 
-    function isClass(element) {
-        return typeof element === "function" &&
-            /^class\s/.test(Function.prototype.toString.call(element))
-    }
-
     function anElement(element, children) {
         if (isClass(element)) {
             const component = new element();
             return component.render();
         }
-        if (typeof element === "function") return element();
+        if (isFunction(element)) return element();
         const anElement = document.createElement(element);
         children.forEach(child => {
             if (typeof child === "object") anElement.appendChild(child);
---
+++ src/utils.js
@@ -0,0 +1,8 @@
+function isClass(element) {
+    return isFunction(element) &&
+        /^class\s/.test(Function.prototype.toString.call(element))
+}
+
+function isFunction(element) {
+    return typeof element === "function";
+}
