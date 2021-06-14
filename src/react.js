f5487634daccc236fcc5fcf4a60f732163b6c908
riyaz <riyazpasha880@gmail.com>
2021-06-14 22:38:26 +0530

    Refactor. Reduced long function into individual small functions

--- src/react.js
+++ src/react.js
@@ -1,17 +1,25 @@
 (() => {
 
-    function anElement(element, children) {
-        if (isClass(element)) {
-            const component = new element();
-            return component.render();
-        }
-        if (isFunction(element)) return element();
+    function handleClass(element) {
+        const component = new element();
+        return component.render();
+    }
+
+    function handleHTMLElement(element, children) {
         const anElement = document.createElement(element);
         children.forEach(child => {
-            if (typeof child === "object") anElement.appendChild(child);
-            else anElement.innerHTML += child;
+            if (typeof child === "object")
+                anElement.appendChild(child);
+            else
+                anElement.innerHTML += child;
         });
         return anElement;
+    }
+
+    function anElement(element, children) {
+        if (isClass(element)) return handleClass(element);
+        if (isFunction(element)) return element();
+        return handleHTMLElement(element, children);
     }
 
     function createElement(element, props, ...children) {
