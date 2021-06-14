4a5d85c4c513814eddc10928fb09da1e50392392
riyaz <riyazpasha880@gmail.com>
2021-06-14 22:30:37 +0530

    Add. Render class component

--- src/react.js
+++ src/react.js
@@ -1,5 +1,15 @@
 (() => {
+
+    function isClass(element) {
+        return typeof element === "function" &&
+            /^class\s/.test(Function.prototype.toString.call(element))
+    }
+
     function anElement(element, children) {
+        if (isClass(element)) {
+            const component = new element();
+            return component.render();
+        }
         if (typeof element === "function") return element();
         const anElement = document.createElement(element);
         children.forEach(child => {
